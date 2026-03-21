7d182834154402b9072b0d6f7b07b9a426064b27
Priyanshu-010 <priyanshurai2772@gmail.com>
2026-03-21 12:17:38 +0530

    fastapi

--- fastapi/basics/main.py
+++ fastapi/basics/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, Request
+from dtos import ProductDTO
 
 app = FastAPI()
+
+products = []
 
 @app.get("/")
 def home():
@@ -9,13 +12,16 @@ def home():
 @app.get("/contact")
 def contact():
   return "Connect with us"
+@app.get("/product")
+def contact():
+  return products
 
 
 @app.get("/product/{product_id}")
-def product(product_id):
-  return {
-    "id": product_id
-  }
+def product(product_id: int):
+  for oneProduct in products:
+    if oneProduct.get("id") == product_id:
+      return oneProduct
 
 @app.get("/greet")
 def greet(request: Request):
@@ -24,3 +30,14 @@ def greet(request: Request):
   return {
     "greet": f"Hello {query_params.get("name")}, Your age is {query_params.get("age")}"
   }
+
+# How to send data: 
+# body, headers - request headers, query_params
+@app.post("/create")
+def create(data: ProductDTO):
+  data = data.model_dump()
+  # print(data)
+  products.append(data)
+  print(product)
+  return {"status": "product created successfully", "data": data}
+
